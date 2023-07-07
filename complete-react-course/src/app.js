239fb85117a80738fbb1a61dc867582d7fbadd55
Muchi Gora <muchigora@gmail.com>
2023-07-07 15:57:55 -0400

    First App Component created

---
+++ complete-react-course/src/app.js
@@ -0,0 +1,4 @@
+function App(){
+    return <h2>This is an MY FIRST component</h2>
+}
+export default App;
--- complete-react-course/src/index.js
+++ complete-react-course/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './app';
 
-const header = <h1>This is my first React App</h1>
+// const header = <h1>This is my first React App</h1>
 
 // console.log(header);
-ReactDOM.render(header, document.querySelectorAll('#root'));
+// ReactDOM.render(header, document.querySelectorAll('#root'));
+
+const root  = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App></App>);
