a479f02cea48a78accea4745314b211bfca9f55b
Muchi Gora <muchigora@gmail.com>
2023-07-10 16:50:34 -0400

    more JSX

--- complete-react-course/src/components/Products.js
+++ complete-react-course/src/components/Products.js
@@ -1,9 +1,20 @@
 
 function Products(){
     return(
-        <div>
-            <h1>All Products</h1>
-        </div>
+        <ul>
+            <li>
+                <div>
+                    <div>
+                        <h5>Fresh Milk</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea et nobis reprehenderit eveniet neque quisquam aliquam facilis beatae, error quae atque veniam deserunt asperiores. Dolor maiores rem sed. Iste, voluptatem.</p>
+                        <div>
+                            <h6>$120.00</h6>
+                        </div>
+                    </div>
+                    <img src="https://picsum.photos/200" width="200" alt="product image"></img>
+                </div>
+            </li>            
+        </ul>
     )
 }
 export default Products;
