5ac0d4ba8fb30a5a161bec9560581b7e4e1f647b
Muchi Gora <muchigora@gmail.com>
2023-07-10 16:40:17 -0400

    child components introduced

--- complete-react-course/src/app.js
+++ complete-react-course/src/app.js
@@ -1,15 +1,13 @@
 import './app.css';
+import './components/Products';
+import Products from './components/Products';
 
 
 function App(){
     let mgs = "AWESOME";   //JS variable
     return (
     <div>
-        <h1 className="myHeader">This is an MY FIRST component</h1>
-
-        <p className="myParagraph">this is my first {mgs} paragraph</p>  {/* JS variable in JSX*/}
-
-        <input type="text"></input>
+        <Products></Products>
 
     </div>
     )
