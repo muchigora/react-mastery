8d7f36a6ef03d197d571a3f7c7d3555cc1bfd11e
Muchi Gora <muchigora@gmail.com>
2023-07-12 01:18:40 -0400

    props

--- complete-react-course/src/app.js
+++ complete-react-course/src/app.js
@@ -2,13 +2,96 @@ import './app.css';
 import './components/Products';
 import Products from './components/Products';
 
+/**Using PROPS to display various product using the Products component*/
+const product = [
+    {
+        pId: 1,
+        pName: "Fresh Milk",
+        pDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        isAvailable: true,
+        image: "images/fresh-milk.png",
+        price: 12
+    },
+    {
+        pId: 2,
+        pName: "Cottage Cheese",
+        pDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac tortor vitae purus faucibus ornare suspendisse sed nisi. Nam at lectus urna duis convallis convallis. Enim lobortis scelerisque fermentum dui faucibus in ornare. ",
+        isAvailable: false,
+        image: "images/cottage-cheese.png",
+        price: 10
+    },
+    {
+        pId: 3,
+        pName: "Brocoli",
+        pDescription: "Purus non enim praesent elementum. Donec ac odio tempor orci dapibus ultrices. Nec tincidunt praesent semper feugiat nibh sed pulvinar proin gravida. Velit egestas dui id ornare arcu odio. Senectus et netus et malesuada fames ac turpis egestas. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu. ",
+        isAvailable: true,
+        image: "images/brocoli.png",
+        price: 15
+    },
+    {
+        pId: 4,
+        pName: "Oranges",
+        pDescription: "Mi ipsum faucibus vitae aliquet nec. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Sed pulvinar proin gravida hendrerit. Porta nibh venenatis cras sed. Id interdum velit laoreet id donec ultrices tincidunt arcu. Morbi quis commodo odio aenean sed adipiscing diam.",
+        isAvailable: true,
+        image: "images/oranges.png",
+        price: 20
+    },
+    {
+        pId: 5, 
+        pName: 'Olive oil', 
+        pDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu.',
+        isAvailable: false,
+        image: "images/olive-oil.png",
+        price: 14
+    }
+];
+
 
 function App(){
    
     return (
     <div className='row'>
         <div className='col-lg-8 mx-auto'>
-            <Products></Products>
+            <Products
+                id={product[0].pId}
+                name={product[0].pName}
+                description={product[0].pDescription}
+                isAvailable={product[0].isAvailable}
+                imageUrl={product[0].image}
+                price={product[0].price}
+            ></Products>
+            <Products
+                id={product[1].pId}
+                name={product[1].pName}
+                description={product[1].pDescription}
+                isAvailable={product[1].isAvailable}
+                imageUrl={product[1].image}
+                price={product[1].price}
+            ></Products>
+            <Products
+                id={product[2].pId}
+                name={product[2].pName}
+                description={product[2].pDescription}
+                isAvailable={product[2].isAvailable}
+                imageUrl={product[2].image}
+                price={product[2].price}
+            ></Products>
+            <Products
+               id={product[3].pId}
+                name={product[3].pName}
+                description={product[3].pDescription}
+                isAvailable={product[3].isAvailable}
+                imageUrl={product[3].image}
+                price={product[3].price}
+            ></Products>
+            <Products
+                id={product[4].pId}
+                name={product[4].pName}
+                description={product[4].pDescription}
+                isAvailable={product[4].isAvailable}
+                imageUrl={product[4].image}
+                price={product[4].price}
+            ></Products>
         </div>
     </div>
     )
--- complete-react-course/src/components/Products.js
+++ complete-react-course/src/components/Products.js
@@ -2,32 +2,34 @@
 let productCount = 0;
 let isAvailable = 'Available'; {/**If availble text background will be green otherwise it will be red */}
 
-let badgeClass = 'badge-margin-left-240 badge '; {/**used to determine whether product is availble or not, and assign a class based on availability.  */}
-badgeClass += isAvailable === 'Available' ? 'bg-success' : 'bg-danger';
-
 
 
 function displayFormattedProductCount(){
     return productCount > 0 ? productCount : <h5>zero</h5>;
 }
 
-function Products(){
+function Products(props){
+
+    let badgeClass = 'badge-margin-left-240 badge '; {/**used to determine whether product is availble or not, and assign a class based on availability.  */}
+        badgeClass += props.isAvailable ? 'bg-success' : 'bg-danger';
+
+
     return(
         <ul className="list-group shadow">
             <li className="list-group-item">
                 <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                     <div className="media-body order-2 order-lg-1">
-                        <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
-                        <p className="font-italic text-muted mb-0 small">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea et nobis reprehenderit eveniet neque quisquam aliquam facilis beatae, error quae atque veniam deserunt asperiores. Dolor maiores rem sed. Iste, voluptatem.</p>
+                        <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
+                        <p className="font-italic text-muted mb-0 small">{props.description}</p>
                         <div className="d-flex align-items-center justify-content-start mt-1">
-                            <h6 className="font-weight-bold my-2" style={{marginRight:20}}>$120.00</h6>
+                            <h6 className="font-weight-bold my-2" style={{marginRight:20}}>{props.price}</h6>
                             <button className="btn btn-primary">-</button>
                             <span style={{padding:"0px 10px", fontSize: 10}}>{displayFormattedProductCount()}</span>  {/**JS expressions can be imedded inside curly braces in a SJX expression */}
                             <button className="btn btn-primary">+</button>
-                            <span className={badgeClass}>{isAvailable}</span> {/**Adding CSS Classes dynamically */}
+                            <span className={badgeClass}>{props.isAvailable ? "Available" : "Unavailable"}</span> {/**Adding CSS Classes dynamically */}
                         </div>
                     </div>
-                    <img src={require("../images/fresh-milk.jpg")} width="200" alt="product image" className="ml-lg-5 order-1 order-lg-2"></img>
+                    <img src={(props.imageUrl)} width="200" alt="product image" className="ml-lg-5 order-1 order-lg-2"></img>
                 </div>
             </li>            
         </ul>
