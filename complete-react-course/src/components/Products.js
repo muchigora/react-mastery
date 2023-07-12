c90460bbea4ba718d66536bbeca59450ffc84e69
Muchi Gora <muchigora@gmail.com>
2023-07-11 23:39:05 -0400

    Adding CSS-classes dynamically

--- complete-react-course/src/components/Products.js
+++ complete-react-course/src/components/Products.js
@@ -1,5 +1,11 @@
 
 let productCount = 0;
+let isAvailable = 'Available'; {/**If availble text background will be green otherwise it will be red */}
+
+let badgeClass = 'badge-margin-left-240 badge '; {/**used to determine whether product is availble or not, and assign a class based on availability.  */}
+badgeClass += isAvailable === 'Available' ? 'bg-success' : 'bg-danger';
+
+
 
 function displayFormattedProductCount(){
     return productCount > 0 ? productCount : <h5>zero</h5>;
@@ -18,6 +24,7 @@ function Products(){
                             <button className="btn btn-primary">-</button>
                             <span style={{padding:"0px 10px", fontSize: 10}}>{displayFormattedProductCount()}</span>  {/**JS expressions can be imedded inside curly braces in a SJX expression */}
                             <button className="btn btn-primary">+</button>
+                            <span className={badgeClass}>{isAvailable}</span> {/**Adding CSS Classes dynamically */}
                         </div>
                     </div>
                     <img src={require("../images/fresh-milk.jpg")} width="200" alt="product image" className="ml-lg-5 order-1 order-lg-2"></img>
