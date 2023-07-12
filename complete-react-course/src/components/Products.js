e702681aae2eeab34ebf21cba30fcf6947166920
Muchi Gora <muchigora@gmail.com>
2023-07-11 20:53:18 -0400

    JSX Attributes

--- complete-react-course/src/components/Products.js
+++ complete-react-course/src/components/Products.js
@@ -14,13 +14,13 @@ function Products(){
                         <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
                         <p className="font-italic text-muted mb-0 small">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea et nobis reprehenderit eveniet neque quisquam aliquam facilis beatae, error quae atque veniam deserunt asperiores. Dolor maiores rem sed. Iste, voluptatem.</p>
                         <div className="d-flex align-items-center justify-content-start mt-1">
-                            <h6 className="font-weight-bold my-2">$120.00</h6>
+                            <h6 className="font-weight-bold my-2" style={{marginRight:20}}>$120.00</h6>
                             <button className="btn btn-primary">-</button>
-                            <span>{displayFormattedProductCount()}</span>  {/**JS expressions can be imedded inside curly braces in a SJX expression */}
+                            <span style={{padding:"0px 10px", fontSize: 10}}>{displayFormattedProductCount()}</span>  {/**JS expressions can be imedded inside curly braces in a SJX expression */}
                             <button className="btn btn-primary">+</button>
                         </div>
                     </div>
-                    <img src="https://picsum.photos/200" width="200" alt="product image" className="ml-lg-5 order-1 order-lg-2"></img>
+                    <img src={require("../images/fresh-milk.jpg")} width="200" alt="product image" className="ml-lg-5 order-1 order-lg-2"></img>
                 </div>
             </li>            
         </ul>
