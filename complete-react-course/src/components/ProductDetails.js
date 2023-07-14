2a6e167ea3ceb1060ea91e766a685ae34715ece5
Muchi Gora <muchigora@gmail.com>
2023-07-14 03:33:24 -0400

    useState

--- complete-react-course/src/components/ProductDetails.js
+++ complete-react-course/src/components/ProductDetails.js
@@ -1,48 +1,31 @@
 import React, {useState}  from "react";
 import Button from "./Button";
 
-/**
- * MORE NOTES
- * useState() 
- * Is a react hook 
- * It register the state or value as a state of a component in which it is called 
- * 
- * Is being called in ProductDetail Component 
- * Registers the value of productCount as 0, initial value
- * As productCount changes, useState records the new state / value in updateState       
- *  
- */
-
 
 function ProductDetails(props){
 
     let badgeClass = 'badge-margin-left-240 badge '; 
         badgeClass += props.isAvailable ? 'bg-success' : 'bg-danger';
 
-    // let productCount = 0; {/**Update the updated value of product count in displayFormatCount function to render on the webpage */}
+    let [productCount, updateCount] = useState(0) 
     
-    let [productCount, updateCount] = useState(0) // use state updates ProductDetails with currentState of variables like productCount after intitalization
-    function displayFormattedProductCount(){   {/**This function checks the quantity available and return either an integer of value of qty of 'string "zero" */}
+    function displayFormattedProductCount(){   
         return productCount > 0 ? productCount : "zero";
     }
 
     //Helper functions
     let incrementProductCount = function(){
-        // productCount++;
-        // console.log(productCount);
         updateCount(++productCount);
     }
 
     let decrementProductCount = function(){
-        // productCount--;
-        // console.log(productCount);
         updateCount(--productCount);
     }
 
     return(
         <div className="d-flex align-items-center justify-content-start mt-1" >
             <h6 className="font-weight-bold my-2" style={{marginRight:20}}>{props.price}</h6>
-            <Button eventHandler = {decrementProductCount}>-</Button> {/**Adding and Event handler to a Custom Element */}
+            <Button eventHandler = {decrementProductCount}>-</Button> 
             <span style={{padding:"0px 10px", fontSize: 10}}>{displayFormattedProductCount()}</span>  
             <Button eventHandler = {incrementProductCount} onClick={decrementProductCount}>+</Button>
             <span className={badgeClass}>{props.isAvailable ? "Available" : "Unavailable"}</span>
