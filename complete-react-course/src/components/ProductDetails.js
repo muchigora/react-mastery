1a2f68f9e186f9606c9c503dbefc36ca40630220
Muchi Gora <muchigora@gmail.com>
2023-07-14 00:50:49 -0400

    useState introduced

--- complete-react-course/src/components/ProductDetails.js
+++ complete-react-course/src/components/ProductDetails.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, {useState}  from "react";
 import Button from "./Button";
+
 
 function ProductDetails(props){
 
     let badgeClass = 'badge-margin-left-240 badge '; 
         badgeClass += props.isAvailable ? 'bg-success' : 'bg-danger';
 
-    let productCount = 0;
+    // let productCount = 0; {/**Update the updated value of product count in displayFormatCount function to render on the webpage */}
+    
+    let [productCount, updateCount] = useState(0) // use state updates ProductDetails with currentState of variables like productCount after intitalization
     function displayFormattedProductCount(){   {/**This function checks the quantity available and return either an integer of value of qty of 'string "zero" */}
         return productCount > 0 ? productCount : "zero";
     }
 
     //Helper functions
     let incrementProductCount = function(){
-        productCount++;
-        console.log(productCount);
+        // productCount++;
+        // console.log(productCount);
+        updateCount(++productCount);
     }
 
     let decrementProductCount = function(){
-        productCount--;
-        console.log(productCount);
+        // productCount--;
+        // console.log(productCount);
+        updateCount(--productCount);
     }
 
     return(
